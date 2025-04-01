f4bf9c8c55be63e809ad43838a1ccfa0a07ef15a
Marcus <marcus.o.chan@gmail.com>
2025-04-01 14:26:55 +1300

    day-4 done

---
+++ day-4/q-4b-fast.py
@@ -0,0 +1,22 @@
+#Compared to hard coded grid logic this is a bit shorter and cleaner
+
+def pattern1(grid):
+    diag1 = grid[0][0] + grid[1][1] + grid[2][2]
+    diag2 = grid[2][0] + grid[1][1] + grid[0][2]
+    return diag1 in {"MAS", "SAM"} and diag2 in {"MAS", "SAM"}
+
+with open("day-4/INPUT4.txt") as f:
+    lines = f.read().strip().splitlines()
+
+total = 0
+for j in range(len(lines)-2):
+    for i in range(len(lines[0])-2):
+        grid = [
+            lines[j][i:i+3],
+            lines[j+1][i:i+3],
+            lines[j+2][i:i+3]
+        ]
+        if pattern1(grid):
+            total += 1
+
+print(total)
---
+++ day-4/q-4b.py
@@ -0,0 +1,48 @@
+#Completed 31/03/2025
+
+def pattern1(grid):
+    if (grid[0][0] == 'M' and
+        grid[0][2] =='M' and
+        grid[1][1] == 'A' and
+        grid [2][0] == 'S' and
+        grid[2][2] == 'S'):
+        return True
+    
+    elif (grid[0][0] == 'S' and
+        grid[0][2] =='M' and
+        grid[1][1] == 'A' and
+        grid [2][0] == 'S' and
+        grid[2][2] == 'M'):
+        return True
+    
+    elif (grid[0][0] == 'S' and
+        grid[0][2] =='S' and
+        grid[1][1] == 'A' and
+        grid [2][0] == 'M' and
+        grid[2][2] == 'M'):
+        return True
+    
+    elif (grid[0][0] == 'M' and
+        grid[0][2] =='S' and
+        grid[1][1] == 'A' and
+        grid [2][0] == 'M' and
+        grid[2][2] == 'S'):
+        return True
+    else:
+        return False
+    
+with open("day-4/INPUT4.txt") as f:
+    lines = f.read().strip().splitlines()
+
+total = 0
+for j in range(len(lines)-2):
+    for i in range(len(lines[0])-2):
+        grid = [
+            lines[j][i:i+3],
+            lines[j+1][i:i+3],
+            lines[j+2][i:i+3]
+        ]
+        if pattern1(grid):
+            total += 1
+
+print(total)
